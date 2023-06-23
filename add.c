14fa37dd4dd6401b6a0248f5d59c9f33193289e0
Abel Sekibaala <abel.sekibaala@gmail.com>
2023-06-23 04:49:17 -0700

    add.c nop.c swap.c

---
+++ add.c
@@ -0,0 +1,28 @@
+#include "monty.h"
+
+/**
+ * add - adds the top two elements of the stack.
+ * @stack: the stack
+ * @line_number: the current line number
+ *
+ * Return: void
+ */
+void add(stack_t **stack, unsigned int line_number)
+{
+int summ;
+
+if ((*stack) == NULL)
+{
+fprintf(stdin, "L%d: can't add, stack too short\n", line_number);
+exit(EXIT_FAILURE);
+}
+if ((*stack)->next == NULL)
+{
+fprintf(stdin, "L%d: can't add, stack too short\n", line_number);
+exit(EXIT_FAILURE);
+}
+
+summ = (*stack)->next->n + (*stack)->n;
+pop(stack, line_number);
+(*stack)->n = summ;
+}
